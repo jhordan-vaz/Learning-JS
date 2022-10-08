a67959be99f9c5749342e2459196cc38e372ea68
Jhordan <jhordanX@gmail.com>
2022-10-08 02:10:05 -0300

    Learning-Js
    .

--- Relational-Database/insertNewStates.sql
+++
@@ -1,7 +0,0 @@
-insert into Estado (id, name, initials, region, population)
-values (1000, 'New', 'NV', 'South', 2.54)
-
-select * from Estado 
-
-insert into Estado (name, initials, region, population)
-values ('Brand New', 'BN', 'North', 2.51)
