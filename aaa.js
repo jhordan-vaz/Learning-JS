e1f0a04f7c453a0623e031f7009c785607bc9997
jhordan-vaz <jhordanX@gmail.com>
2023-01-13 02:28:35 -0300

    Learning-JS
    .

---
+++ aaa.js
@@ -0,0 +1,14 @@
+const cmd = require("prompt-sync")()
+
+objeto1 = {name: "jhordan, shep, samson, loli"};
+objeto2 = {idade: "23, 32, 30, 20"};
+
+console.log(objeto1)
+console.log(objeto2)
+
+if (objeto1 > null) {
+    console.log("Vázio!")
+} else {
+    console.log("Cheio")
+}
+
